5ecb2cc3108caf346b801c515d7165e675ca584b
tangguanglei <tangguanglei@oneship.cc>
2019-09-25 09:52:09 +0800

    two sum 1

---
+++ algorithm-leetcode/src/main/java/com/tangguanglei/array/TwoSum1.java
@@ -0,0 +1,28 @@
+package com.tangguanglei.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ */
+public class TwoSum1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        //define a map to save nums and indices in array
+        Map<Integer, Integer> arrMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            arrMap.put(nums[i], i);
+        }
+        //loop the array to calc the target
+        for (int j = 0; j < nums.length; j++) {
+            int k = target - nums[j];
+            if (arrMap.containsKey(k) && j != arrMap.get(k)) {
+                return new int[]{arrMap.get(k), j};
+            }
+        }
+        return null;
+    }
+}
---
+++ algorithm-leetcode/src/test/java/com/tangguanglei/array/TwoSum1Test.java
@@ -0,0 +1,15 @@
+package com.tangguanglei.array;
+
+import org.junit.Test;
+
+public class TwoSum1Test {
+
+    @Test
+    public void twoSum() {
+        TwoSum1 twoSum1 = new TwoSum1();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum1.twoSum(nums, target);
+        assert result[0] == 1 && result[1] == 0;
+    }
+}
