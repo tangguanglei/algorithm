f3db32d43a67b800845716ffcf5d1c2a2dac3179
tangguanglei <tguang1ei@gmail.com>
2023-10-25 16:29:28 +0800

    数据结构&排序算法

--- algorithm-common/src/test/java/com/tangguanglei/Solution.java
+++ algorithm-common/src/test/java/com/tangguanglei/Solution.java
@@ -1,51 +1,60 @@
 package com.tangguanglei;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
-class Solution {
-    List<List<Integer>> edges;
-    int[] indeg;
+public class Solution {
 
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        edges = new ArrayList<>();
-        for (int i = 0; i < numCourses; ++i) {
-            edges.add(new ArrayList<>());
-        }
-        indeg = new int[numCourses];
-        for (int[] info : prerequisites) {
-            edges.get(info[1]).add(info[0]);
-            ++indeg[info[0]];
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; ++i) {
-            if (indeg[i] == 0) {
-                queue.offer(i);
-            }
+        int maxLen = 1;
+        int begin = 0;
+        // dp[i][j] 表示 s[i..j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        // 初始化：所有长度为 1 的子串都是回文串
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
         }
 
-        int visited = 0;
-        while (!queue.isEmpty()) {
-            ++visited;
-            int u = queue.poll();
-            for (int v: edges.get(u)) {
-                --indeg[v];
-                if (indeg[v] == 0) {
-                    queue.offer(v);
+        char[] charArray = s.toCharArray();
+        // 递推开始
+        // 先枚举子串长度
+        for (int L = 2; L <= len; L++) {
+            // 枚举左边界，左边界的上限设置可以宽松一些
+            for (int i = 0; i < len; i++) {
+                // 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+                int j = L + i - 1;
+                // 如果右边界越界，就可以退出当前循环
+                if (j >= len) {
+                    break;
+                }
+
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
                 }
             }
         }
-
-        return visited == numCourses;
+        return s.substring(begin, begin + maxLen);
     }
 
     public static void main(String[] args) {
-        int numCourses = 3;
-        int[][] prerequisites = new int[][]{{0, 1}, {0, 2}};
+        String s = "ababa";
         Solution solution = new Solution();
-        System.out.println(solution.canFinish(numCourses, prerequisites));
+        solution.longestPalindrome(s);
     }
 }
