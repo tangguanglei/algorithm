4e45ef67ecc90ef08635400a6f1b58518c56a1b7
tangguanglei <tguang1ei@gmail.com>
2023-11-16 08:18:36 +0800

    数据结构&排序算法

---
+++ algorithm-leetcode/src/main/java/com/tangguanglei/matrix/旋转图像48.java
@@ -0,0 +1,27 @@
+package com.tangguanglei.matrix;
+
+public class 旋转图像48 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        旋转图像48 solution = new 旋转图像48();
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        solution.rotate(matrix);
+    }
+}
---
+++ algorithm-leetcode/src/main/java/com/tangguanglei/matrix/螺旋矩阵54.java
@@ -0,0 +1,36 @@
+package com.tangguanglei.matrix;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 螺旋矩阵54 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> order = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return order;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        boolean[][] visited = new boolean[rows][columns];
+        int total = rows * columns;
+        int row = 0, column = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int directionIndex = 0;
+        for (int i = 0; i < total; i++) {
+            order.add(matrix[row][column]);
+            visited[row][column] = true;
+            int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+            if (nextRow < 0 || nextRow >= rows || nextColumn < 0 || nextColumn >= columns || visited[nextRow][nextColumn]) {
+                directionIndex = (directionIndex + 1) % 4;
+            }
+            row += directions[directionIndex][0];
+            column += directions[directionIndex][1];
+        }
+        return order;
+    }
+
+    public static void main(String[] args) {
+        螺旋矩阵54 solution = new 螺旋矩阵54();
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        solution.spiralOrder(matrix);
+    }
+}
