9f40a9b206ea1a3af0e16cbbe6cab5f49f32ef0e
tangguanglei <tguang1ei@gmail.com>
2024-08-28 10:53:54 +0800

    数据结构&排序算法

--- algorithm-leetcode/src/main/java/com/tangguanglei/hash/最长连续序列128.java
+++ algorithm-leetcode/src/main/java/com/tangguanglei/hash/最长连续序列128.java
@@ -11,15 +11,16 @@ public class 最长连续序列128 {
         }
         int longestConsecutive = 0;
         for (int num : nums) {
-            if (!set.contains(num - 1)) {
-                int currentNum = num;
-                int currentConsecutive = 1;
-                while (set.contains(currentNum + 1)) {
-                    currentNum += 1;
-                    currentConsecutive += 1;
-                }
-                longestConsecutive = Math.max(longestConsecutive, currentConsecutive);
+            if (set.contains(num - 1)) {
+                continue;
             }
+            int currentNum = num;
+            int currentConsecutive = 1;
+            while (set.contains(currentNum + 1)) {
+                currentNum += 1;
+                currentConsecutive += 1;
+            }
+            longestConsecutive = Math.max(longestConsecutive, currentConsecutive);
         }
         return longestConsecutive;
     }
