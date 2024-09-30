d974f2771b963e494c19ffc07662af56f31a3804
tangguanglei <tguang1ei@gmail.com>
2024-09-30 15:27:37 +0800

    数据结构&排序算法

--- algorithm-leetcode/src/main/java/com/tangguanglei/graph/课程表207.java
+++ algorithm-leetcode/src/main/java/com/tangguanglei/graph/课程表207.java
@@ -7,39 +7,40 @@ import java.util.Queue;
 
 public class 课程表207 {
     List<List<Integer>> edges;
-    int[] indeg;
+    boolean valid = true;
+    int[] visited;
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         edges = new ArrayList<>();
-        for (int i = 0; i < numCourses; ++i) {
+        for (int i = 0; i < numCourses; i++) {
             edges.add(new ArrayList<>());
         }
-        indeg = new int[numCourses];
-        for (int[] info : prerequisites) {
-            edges.get(info[1]).add(info[0]);
-            ++indeg[info[0]];
+        visited = new int[numCourses];
+        for (int[] course : prerequisites) {
+            edges.get(course[0]).add(course[1]);
         }
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; ++i) {
-            if (indeg[i] == 0) {
-                queue.offer(i);
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0) {
+                dfs(i);
             }
         }
+        return valid;
+    }
 
-        int visited = 0;
-        while (!queue.isEmpty()) {
-            ++visited;
-            int u = queue.poll();
-            for (int v: edges.get(u)) {
-                --indeg[v];
-                if (indeg[v] == 0) {
-                    queue.offer(v);
+    private void dfs(int u) {
+        visited[u] = 1;
+        for (int v : edges.get(u)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
                 }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
             }
         }
-
-        return visited == numCourses;
+        visited[u] = 2;
     }
 
     public static void main(String[] args) {
