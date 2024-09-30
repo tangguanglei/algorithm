9fabc9ace3e0b31b6dfd36a380d49e50cb435703
tangguanglei <tguang1ei@gmail.com>
2024-09-30 11:35:09 +0800

    数据结构&排序算法

---
+++ algorithm-leetcode/src/main/java/com/tangguanglei/graph/Trie.java
@@ -0,0 +1,47 @@
+package com.tangguanglei.graph;
+
+public class Trie {
+    Trie[] children;
+    boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd == true;
+    }
+
+    public boolean startsWith(String prefix) {
+        Trie node = searchPrefix(prefix);
+        return node != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+}
